4ed3a73e324c2eaece2e4d0072c615fcb77c21a6
jeljov <jeljov@gmail.com>
2025-06-19 14:26:25 +0200

    minor updates in kmeans

--- kmeans/clustering_util.R
+++ kmeans/clustering_util.R
@@ -2,7 +2,10 @@
 ## UTILITY FUNCTIONS #
 ######################
 
-## function that provides summary statistics about clusters
+## Function that provides summary statistics (mean and st. deviation) about clusters
+## Input arguments:
+## - features: a data frame of features (attributes) for which summary statistics should be computed
+## - clusters: a vector of cluster assignments
 summary_stats <- function(features, clust) {
   require(dplyr)
   require(tidyr)
@@ -31,14 +34,16 @@ summary_stats <- function(features, clust) {
 ## box plots, one plot for each attribute that was used for clustering.
 ## The purpose is to visually compare the distribution of the attributes
 ## across the clusters
-# 
-## The 'main' function is create_comparison_plots() that receives 2 input parameters:
-## - a data frame with the attributes used for clustering
-## - a vector of cluster assignments
-## The function creates and arranges box plots for all the attributes.
-# 
-## The create_attr_boxplot() is a helper function for the create_comparison_plots() f.
-## that creates a box plot for one attribute
+## 
+## The 'main' function is create_comparison_plots(), which creates and arranges 
+## box plots for all the attributes. The create_attr_boxplot() is a helper function 
+## that creates a box plot for one attribute.
+## 
+## The create_comparison_plots() receives 3 input arguments:
+## - df: a data frame with the attributes to be used for cluster comparison
+## - clust: a vector of cluster assignments
+## - ncol: number of box plots to be presented in one row (by default, 3)
+
 create_attr_boxplot <- function(df, attribute, clust_var) {
   ggplot(data = df,
          mapping = aes(x=.data[[clust_var]], 
